01b01e81b5b75619bc623eb54668b0e963f6143d
Nur Chamdani <kokme.km@gmail.com>
2024-03-20 18:55:26 +0700

    update files

--- lib/jwt.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:flutter_secure_storage/flutter_secure_storage.dart'; // 
-
-// final jwt = await _storage.read(key: 'jwt');
-// final response = await http.get(
-//   Uri.parse('http://94.74.86.174:8080/protected-endpoint'),
-//   headers: {
-//     'Authorization': 'Bearer $jwt',
-//   },
-// );
